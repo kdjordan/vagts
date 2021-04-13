f6b8b59474499a8c5c614669e376adab89875837
kdjordan <k.dean.jordan@gmail.com>
2021-04-13 14:15:20 -0700

    first step

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Logo />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Logo from './components/Logo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Logo
   }
 }
 </script>
---
+++ src/components/Logo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="logo">
+    <img alt="Vue logo" src="../assets/img/logo.svg">
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.logo {
+  display: flex;
+  height: 100vh;
+  width: 50vw;
+  align-content: center;
+  justify-content: center;
+}
+
+.logo img  {
+  height: 100px;
+}
+
+</style>
