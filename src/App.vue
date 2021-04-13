798104b095b2d526ec38dc7ca51c5c9896586e9e
kdjordan <k.dean.jordan@gmail.com>
2021-04-13 15:15:14 -0700

    FEAT: logo animated

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,22 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Lora&display=swap');
+
+:root {
+  --green: hsl(155deg 16% 76%);
+  --sand: hsl(59deg 6% 70%);
+  --grey: hsl(59deg 1% 84%);
+  }
+
+body {
+  background-color: var(--grey);
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Lora', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
